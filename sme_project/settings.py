874a3eda56e8ec79847a63cf7ceaa30d5ae4210d
Subham Pattnaik <subham12pc@gmail.com>
2026-09-23 23:39:01 +0530

    Connect Django to PostgreSQL

--- sme_project/settings.py
+++ sme_project/settings.py
@@ -3,6 +3,7 @@ Django settings for sme_project project.
 """
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,10 +54,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sme_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
